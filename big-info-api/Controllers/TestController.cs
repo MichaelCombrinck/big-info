775dc1f44e6d206d5aa55d69762101586dcd52ae
Michael Combrinck <119591159+MichaelCombrinck@users.noreply.github.com>
2024-01-04 12:36:48 +0200

    initial commit

---
+++ big-info-api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace big_info_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Index()
+        {
+            return new string[] { "value 1", "value 2" };
+        }
+    }
+}
