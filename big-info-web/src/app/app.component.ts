317399b6b787399bc076cc8a6f85520340d306aa
Michael Combrinck <119591159+MichaelCombrinck@users.noreply.github.com>
2024-01-04 15:25:45 +0200

    feat(login): page created

--- big-info-web/src/app/app.component.ts
+++ big-info-web/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoginPageComponent } from "../pages/login-page/login-page.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, LoginPageComponent]
 })
 export class AppComponent {
   title = 'big-info-web';
